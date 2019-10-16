5ae3362c175fce193bc0fa9bfda440c27c8a5185
Andrzej Hosz <andrzej.hosz@gmail.com>
2019-10-16 19:51:06 +0200

    SortingInt

---
+++ SortingInt/SortingInt/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace SortingInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę:");
+            string num = (Console.ReadLine());
+            if (num.Length > 9)
+            {
+                Console.WriteLine("Podałeś zbyt dużą liczbę");
+                Console.WriteLine("Podaj liczbę:");
+                num = (Console.ReadLine());
+            }
+            Console.WriteLine("Wybierz sortowanie :");
+            Console.WriteLine("1 - rosnące");
+            Console.WriteLine("2 - malejące");
+            var sel = Console.ReadLine();
+            switch (sel)
+            {
+                case "1": ;
+                    int wynikdesc = AscendingOrder(int.Parse(num));
+                    Console.WriteLine(wynikdesc);
+                    break;
+                case "2": ;
+                    int wynikasc = DescendingOrder(int.Parse(num));
+                    Console.WriteLine(wynikasc);
+                    break;
+            }
+            Console.ReadKey();
+        }            
+
+        public static int DescendingOrder(int num)
+        {
+            var orderlist = num.ToString().OrderByDescending(n => n);
+            return int.Parse(String.Concat(orderlist));
+        }
+
+        public static int AscendingOrder(int num)
+        {
+            var orderlist = num.ToString().OrderBy(n => n);
+            return int.Parse(String.Concat(orderlist));
+        }
+    }
+}
