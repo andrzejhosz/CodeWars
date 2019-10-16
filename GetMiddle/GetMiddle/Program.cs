fc61a0d2524f7b084797f2bdaae11976669ca664
Andrzej Hosz <andrzej.hosz@gmail.com>
2019-10-16 17:13:16 +0200

    GetMiddle

---
+++ GetMiddle/GetMiddle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GetMiddle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a word:");
+            string s = Console.ReadLine();
+            Console.WriteLine("Your words middle char is :" + GetMiddle(s));
+            Console.ReadKey();
+        }
+
+        public static string GetMiddle(string s)
+        {
+            string middle;
+
+            if (s.Length % 2 == 0)
+            {
+                middle = s.Substring(s.Length / 2 - 1, 2);
+
+                return middle;
+            }
+            else
+            {
+                middle = s.Substring(s.Length / 2, 1);
+
+                return middle;
+            }
+        }
+    }
+}
