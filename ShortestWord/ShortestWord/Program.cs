9ae8ef92a16d472509600587a6a7063b2e219a37
Andrzej Hosz <andrzej.hosz@gmail.com>
2019-10-23 18:42:18 +0200

    ShortestWord

---
+++ ShortestWord/ShortestWord/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ShortestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindShort("Tutaj wpisz swój tekst a znajdę Ci długość najkrótszego słowa"));
+            Console.ReadKey();
+        }
+
+        public static int FindShort(string s)
+        {
+           String [] elements = s.Split();
+           int [] wordlength = new int[elements.Length];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                wordlength[i] = elements[i].Length;
+                
+            }
+
+            return wordlength.Min();
+        }
+    }
+}
