fae3007a5604006768c8ba33448a38986eb422bf
Andrzej Hosz <andrzej.hosz@gmail.com>
2019-10-30 18:12:46 +0100

    Line

---
+++ Line/Line/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Tickets(new int[] { 25, 25, 50, 50, 100 }));
+            Console.ReadKey();
+        }
+
+        
+        public static string Tickets(int[] peopleInLine)
+        {
+            int cashAvailable = 0;
+            for (int i = 1; i < peopleInLine.Length; i++)
+            {
+                if (peopleInLine[0] > 25)
+                {
+                    return "NO";
+                }
+
+                cashAvailable = cashAvailable + peopleInLine[i-1]; 
+                 
+                if (cashAvailable < peopleInLine[i])
+                {
+                    return "NO";
+                }
+
+                if (peopleInLine[i] > 25)
+                {
+                    cashAvailable = cashAvailable - peopleInLine[i] + 25;
+                }
+            }
+
+            return "YES";
+        }
+    }
+}
